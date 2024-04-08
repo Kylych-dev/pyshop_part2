6fa2a38a3c591a08708e0890ef131b7e8d1191c8
kylych-dev <mirbekov1kylych@gmail.com>
2024-04-09 01:15:09 +0600

    version python3.9 add django

--- build_files.sh
+++ build_files.sh
@@ -7,8 +7,10 @@ pip install django djangorestframework
 python3.9 -m pip install -r requirements.txt
 
 # echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+find . -path "*/migrations/*.py" -not -path "*/site-packages/*" -delete
+find . -type d -name "__pycache__" -exec rm -rf {} +
+python3.9 manage.py migrate --run-syncdb --noinput&& 
+python3.9 manage.py makemigrations --noinput && 
 
 # echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
