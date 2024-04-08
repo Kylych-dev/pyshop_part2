89ae5b3793cc2e103c5989b4bc69f7f5e9c8687b
kylych-dev <mirbekov1kylych@gmail.com>
2024-04-09 01:30:44 +0600

    version python3.9 add django

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,43 @@
-# Build the project
-# echo "Building the project..."
+# # Build the project
+# # echo "Building the project..."
+# python3.9 -m venv env
+# source env/bin/activate
+
+# # pip3 install --upgrade pip
+# # pip3 install django djangorestframework
+# # pip3 install -r requirements.txt
+
+# pip install --upgrade pip
+# pip install django djangorestframework
+# pip install -r requirements.txt
+
+# # echo "Make Migration..."
+# find . -path "*/migrations/*.py" -not -path "*/site-packages/*" -delete
+# find . -type d -name "__pycache__" -exec rm -rf {} +
+# python3.9 manage.py makemigrations accounts
+# python3.9 manage.py migrate --run-syncdb --noinput
+# python3.9 manage.py makemigrations --noinput
+
+# # echo "Collect Static..."
+# python3.9 manage.py collectstatic --noinput --clear
+
+
+
+# Создание виртуального окружения и активация
 python3.9 -m venv env
 source env/bin/activate
-pip3 install --upgrade pip
-pip3 install django djangorestframework
-pip3 install -r requirements.txt
 
-# echo "Make Migration..."
+# Обновление pip и установка зависимостей
+pip install --upgrade pip
+pip install django djangorestframework
+pip install -r requirements.txt
+
+# Удаление старых миграций и очистка __pycache__
 find . -path "*/migrations/*.py" -not -path "*/site-packages/*" -delete
 find . -type d -name "__pycache__" -exec rm -rf {} +
+
+# Выполнение миграций и сборка статических файлов
 python3.9 manage.py makemigrations accounts
 python3.9 manage.py migrate --run-syncdb --noinput
 python3.9 manage.py makemigrations --noinput
-
-# echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
