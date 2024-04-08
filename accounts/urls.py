312ff76bd5a2111b5bf67ed513f17ce74ec7f30d
kylych-dev <mirbekov1kylych@gmail.com>
2024-04-08 17:14:54 +0600

    .

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,6 @@ from accounts.views import (
     AuthenticationView,
     RegisterView,
     UserView,
-    LogoutView,
-    protected_view,
     )
 
 router = DefaultRouter(trailing_slash=False)
@@ -17,43 +15,7 @@ urlpatterns.extend(
     [   
         path('register/', RegisterView.as_view({"post": "register"}), name='register'),
         path('login/', AuthenticationView.as_view({"post": "login"}), name='login'),
-        path('logout/', LogoutView.as_view(), name='logout'),
+        path('logout/', AuthenticationView.as_view({"post": "logout"}), name='logout'),
         path('user/', UserView.as_view()),
-        path('protected/', protected_view, name='protected_view'),
-
     ]
 )
-
-
-
-'''
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-
-from .views import RegisterView, UserAuthenticationView
-
-
-# router = DefaultRouter(trailing_slash=False)
-# urlpatterns = router.urls
-
-
-
-
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', UserAuthenticationView.as_view(), name='login'),
-    path('logout/', UserAuthenticationView.as_view(), name='logout'),
-
-]'''
-
-
-'''
-urlpatterns = [
-    # Регистрация
-    path("register/", RegisterView.as_view(), name="register"),
-    
-    # Логин и выход
-    path("login/", UserAuthenticationView.as_view(), name="login"),
-    path("logout/", UserAuthenticationView.as_view(), name="logout")
-]'''
--- accounts/views.py
+++ accounts/views.py
@@ -78,64 +78,6 @@ class RegisterView(viewsets.ViewSet):
 
 
 
-# class LoginView(APIView):
-#     def post(self, request):
-#         email = request.data['email']
-#         password = request.data['password']
-
-#         user = CustomUser.objects.filter(email=email).first()
-
-#         if user is None:
-#             raise AuthenticationFailed('User not found!')
-
-#         if not user.check_password(password):
-#             raise AuthenticationFailed('Incorrect password!')
-
-#         payload = {
-#             'id': user.id,
-#             'exp': datetime.utcnow() + timedelta(minutes=60),
-#             'iat': datetime.utcnow()
-#         }
-
-#         # token = jwt.encode(payload, 'secret', algorithm='HS256')
-#         token = jwt.encode(payload, 'secret', algorithm='HS256')
-
-
-#         response = Response()
-
-#         response.set_cookie(key='jwt', value=token, httponly=True)
-#         response.data = {
-#             'jwt': token
-#         }
-#         return response
-
-
-
-'''
-
-class UserView(APIView):
-
-
-
-    def get(self, request):
-        token = request.COOKIES.get('jwt')
-
-        if not token:
-            raise AuthenticationFailed('Unauthenticated!')
-
-        try:
-            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
-        except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed('Unauthenticated!')
-
-        user = CustomUser.objects.filter(id=payload['id']).first()
-        serializer = CustomUserSerializer(user)
-        return Response(serializer.data)
-        
-'''
-
-
-
 class AuthenticationView(viewsets.ViewSet):
     @swagger_auto_schema(
         operation_description="Авторизация пользователя для получения токена.",
@@ -156,7 +98,6 @@ class AuthenticationView(viewsets.ViewSet):
             404: openapi.Response(description="Not Found - Пользователь не найден"),
         },
     )
-    
     def login(self, request):
         email = request.data.get("email")
         password = request.data.get("password")
@@ -188,6 +129,24 @@ class AuthenticationView(viewsets.ViewSet):
             status=status.HTTP_200_OK,
         )
     
+    @swagger_auto_schema(
+        operation_description="Выход для удаления токена.",
+        operation_summary="Выход для удаления токена",
+        operation_id="logout_user",
+        tags=["Authentication"],
+        responses={
+            201: openapi.Response(description="OK - Выход пользователя прошел успешно."),
+            400: openapi.Response(description="Bad Request - Неверный запрос."),
+        },
+    )
+    def logout(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'success'
+        }
+        return response
+    
 
 class UserView(APIView):
     # permission_classes = [permissions.IsAuthenticated,]
@@ -212,166 +171,3 @@ class UserView(APIView):
         return Response(serializer.data)
 
 
-
-
-
-'''   
-    
-        
-        access_token = generate_access_token(user.id)
-        # refresh_token = generate_refresh_token(user.id)
-
-        refresh_token_obj, created = RefreshToken.objects.get_or_create(user=user)
-        # refresh_token_obj.token = refresh_token
-        # refresh_token_obj.expires_at = refresh_token_exp
-        refresh_token_obj.save()
-        
-
-        return access_token
-        
-        
-    '''
-
-        # return Response(
-        #     data={
-        #         "access_token": str(access_token),
-        #         # "refresh_token": str(refresh_token),
-                
-        #         "email": user.email,
-        #     },
-        #     status=status.HTTP_200_OK,
-        # )
-        
-
-        # offset = datetime.timedelta(hours=-8)
-
-        # payload = {
-        #     'id': user.id,
-        #     'exp': datetime.datetime.utcnow() + offset,
-        #     'iat': datetime.datetime.utcnow()
-        # }
-
-
-        # payload = {
-        #     'id': user.id,
-        #     'exp': datetime.datetime.utcnow() + datetime.timedelta('utc-8'),
-        #     'iat': datetime.datetime.utcnow()
-        # }
-
-        # token = jwt.encode(payload, 'secret', algorithm='HS256')
-
-        # response = Response()
-
-        # response.set_cookie(key='jwt', value=token, httponly=True)
-        # response.data = {
-        #     'jwt': token
-        # }
-        # return response
-
-
-    
-
-# class UserView(APIView):
-
-#     def get(self, request):
-#         print('request ---->', request.COOKIES)
-#         token = request.COOKIES.get('jwt')
-#         print('token ==================', token)
-
-#         if not token:
-#             print('++++++')
-#             raise AuthenticationFailed('Unauthenticated!')
-
-#         try:
-#             payload = jwt.decode(token, 'secret', algorithm=['HS256'])
-
-#         except jwt.ExpiredSignatureError:
-#             print('******')
-#             raise AuthenticationFailed('Unauthenticated!')
-
-#         user = CustomUser.objects.filter(id=payload['id']).first()
-#         serializer = CustomUserSerializer(user)
-#         return Response(serializer.data)
-
-
-class LogoutView(APIView):
-    def post(self, request):
-        response = Response()
-        response.delete_cookie('jwt')
-        response.data = {
-            'message': 'success'
-        }
-        return response
-
-
-
-        '''
-        
-        access_token = AccessToken.for_user(user)
-        refresh_token = RefreshToken.for_user(user)
-
-        # ___________________________________________________________
-        # Допольнительная ифнормация о токене
-        access_token_lifetime = api_settings.ACCESS_TOKEN_LIFETIME
-        refresh_token_lifetime = api_settings.REFRESH_TOKEN_LIFETIME
-        current_datetime = datetime.now()
-        access_token_expiration = current_datetime + access_token_lifetime
-        refresh_token_expiration = current_datetime + refresh_token_lifetime
-        # ___________________________________________________________
-
-        return Response(
-            data={
-                "access_token": str(access_token),
-                "access_token_expires": access_token_expiration.strftime("%Y-%m-%d %H:%M:%S"),
-
-                "refresh_token": str(refresh_token),
-                "refresh_token_expires": refresh_token_expiration.strftime("%Y-%m-%d %H:%M:%S"),
-                
-                "email": user.email,
-            },
-            status=status.HTTP_200_OK,
-        )
-        
-        '''
-
-    @swagger_auto_schema(
-        operation_description="Выход для удаления токена.",
-        operation_summary="Выход для удаления токена",
-        operation_id="logout_user",
-        tags=["Authentication"],
-        responses={
-            201: openapi.Response(description="OK - Выход пользователя прошел успешно."),
-            400: openapi.Response(description="Bad Request - Неверный запрос."),
-        },
-    )
-    def logout(self, request):
-        refresh_token = request.data.get("refresh_token")
-        if not refresh_token:
-            return Response(
-                data={"error": "Refresh token is required."}, 
-                status=status.HTTP_400_BAD_REQUEST
-                )
-        try:
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-        except TokenError as ex:
-            log_error(self, ex)
-            raise AuthenticationFailed("Invalid token.")
-
-        return Response(
-            "Logged out successfully", 
-            status=status.HTTP_200_OK
-            )  
-    
-
-
-
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
-
-@api_view(['GET'])
-@authentication_classes([JWTAuthentication])
-@permission_classes([IsAuthenticated])
-def protected_view(request):
-    return Response({"message": "This is a protected view."})
