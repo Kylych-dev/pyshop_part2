bd867b833b472654d6e318c34dbae860970184d1
kylych-dev <mirbekov1kylych@gmail.com>
2024-04-09 06:37:52 +0600

    remove now.sh from settings

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-n5nlw!*&vsjd#8c(10j(vkxy&*(%bn7(&0ig=vyu7k(dwc&w8a
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['now.sh', '127.0.0.1', 'localhost', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 
 INSTALLED_APPS = [
