90bc930e9f4d79a9aea571ec5ad54c3a28b51923
kylych-dev <mirbekov1kylych@gmail.com>
2024-04-09 12:30:00 +0600

    ++++

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,10 @@
 python3.9 -m venv env
 source env/bin/activate
-print('++++++++++++++++++++++++++++++++++++++++++')
 pip3 install --upgrade pip
 pip3 install django djangorestframework
-print('++++++++++++++++++++++++++++++++++++++++++')
 pip3 install -r requirements.txt 
 python3.9 manage.py collectstatic --noinput --clear
 
-print('++++++++++++++++++++++++++++++++++++++++++')
 
 # # Build the project
 # # echo "Building the project..."
