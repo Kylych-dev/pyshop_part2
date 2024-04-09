44bb4bec7ceb8e61d5c2fdf913ec9a12c9af64ce
kylych-dev <mirbekov1kylych@gmail.com>
2024-04-09 06:41:59 +0600

    add

--- build_files.sh
+++ build_files.sh
@@ -1,25 +1,25 @@
 # # Build the project
 # # echo "Building the project..."
-# python3.9 -m venv env
-# source env/bin/activate
+python3.9 -m venv env
+source env/bin/activate
 
-# # pip3 install --upgrade pip
-# # pip3 install django djangorestframework
-# # pip3 install -r requirements.txt
+# pip3 install --upgrade pip
+# pip3 install django djangorestframework
+# pip3 install -r requirements.txt
 
-# pip install --upgrade pip
-# pip install django djangorestframework
-# pip install -r requirements.txt
+pip install --upgrade pip
+pip install django djangorestframework
+pip install -r requirements.txt
 
-# # echo "Make Migration..."
-# find . -path "*/migrations/*.py" -not -path "*/site-packages/*" -delete
-# find . -type d -name "__pycache__" -exec rm -rf {} +
-# python3.9 manage.py makemigrations accounts
-# python3.9 manage.py migrate --run-syncdb --noinput
-# python3.9 manage.py makemigrations --noinput
+# echo "Make Migration..."
+find . -path "*/migrations/*.py" -not -path "*/site-packages/*" -delete
+find . -type d -name "__pycache__" -exec rm -rf {} +
+python3.9 manage.py makemigrations accounts
+python3.9 manage.py migrate --run-syncdb --noinput
+python3.9 manage.py makemigrations --noinput
 
-# # echo "Collect Static..."
-# python3.9 manage.py collectstatic --noinput --clear
+# echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
 
 
 
@@ -50,18 +50,18 @@
 
 
 
-# Активация виртуального окружения
-source env/bin/activate
+# # Активация виртуального окружения
+# source env/bin/activate
 
-# Обновление pip и установка зависимостей
-pip install --upgrade pip
-pip install -r requirements.txt
+# # Обновление pip и установка зависимостей
+# pip install --upgrade pip
+# pip install -r requirements.txt
 
-# Удаление старых миграций и очистка __pycache__
-# find . -path "*/migrations/*.py" -not -path "*/site-packages/*" -delete
-# find . -type d -name "__pycache__" -exec rm -rf {} +
+# # Удаление старых миграций и очистка __pycache__
+# # find . -path "*/migrations/*.py" -not -path "*/site-packages/*" -delete
+# # find . -type d -name "__pycache__" -exec rm -rf {} +
 
-# Выполнение миграций и сборка статических файлов
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic
+# # Выполнение миграций и сборка статических файлов
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py collectstatic
