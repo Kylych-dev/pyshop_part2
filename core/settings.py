18baaa1045fad211e7a0786118707803915dfbd1
kylych-dev <mirbekov1kylych@gmail.com>
2024-04-09 13:54:14 +0600

    codind with kazim static

--- core/settings.py
+++ core/settings.py
@@ -137,10 +137,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = BASE_DIR / 'static'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
